92b4fccc7243b3e75addad519b35219f0a3aebb1
Андрей Боярский <an0982978490@outlook.com>
2022-04-25 11:50:51 +0300

    Добавьте файлы проекта.

---
+++ recursion  1 zadaniy/recursion  1 zadaniy.cpp
@@ -0,0 +1,20 @@
+﻿//1. От 1 до n.
+//Дано натуральное число n.Выведите все числа от 1 до n.
+//Ввод: 5
+//Вывод : 1 2 3 4 5
+#include <iostream>
+using namespace std;
+void funk_hisla(int n) {
+	if (n) {
+		funk_hisla(n - 1);
+		cout << n << endl;
+	}
+}
+
+int main()
+{
+	int n = 5;
+	funk_hisla(5);
+	cin >> n;
+}
+
